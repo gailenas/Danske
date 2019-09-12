470a5b0c56e8074bbf0274567a9d34753cf1a0ca
Gailius Besusparis <gailius.besusparis@bazaarvoice.com>
2019-09-12 20:44:52 +0300

    Updates added

---
+++ Update.bat
@@ -0,0 +1,3 @@
+cd C:\Users\%username%\Desktop\Danske\startup
+start AngularUpdate.bat
+start NodeUpdate.bat
---
+++ startup/AngularUpdate.bat
@@ -0,0 +1,4 @@
+cd C:\Users\%username%\Desktop\Danske\NumberPlatesApp\NumberPlates
+npm install
+
+
